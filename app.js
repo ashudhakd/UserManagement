d0c200d57f9213ceedca42f51c8c57048410125b
ashudhakd <80048694+ashudhakd@users.noreply.github.com>
2025-01-22 10:35:13 +0530

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const apiUrl = "https://jsonplaceholder.typicode.com/users"; // Mock API URL
+const apiUrl = "https://jsonplaceholder.typicode.com/users"; 
 
 // Elements
 const addUserBtn = document.getElementById("addUserBtn");
